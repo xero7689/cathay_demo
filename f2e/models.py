1d75772fa5438a2164f9864869bce2a22447f233
xero7689 <volleyp7689@gmail.com>
2020-02-03 23:42:41 +0800

    Add f2e models

--- f2e/models.py
+++ f2e/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Order(models.Model):
+    name = models.TextField()
+    logo = models.URLField()
+    status = models.IntegerField()
+    date = models.DateField()
--- f2e/tests.py
+++ f2e/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Order
 
-# Create your tests here.
+class OrdersTestCase(TestCase):
+    def test_saving_and_retrieving(self):
+        order = Order()
+        order.name = 'Apple Airpods 2'
+        order.logo = 'https://static.oopocket.com/store/iconTreemall@3x.png'
+        order.status = 4
+        order.date = '2019-03-02'
+        order.save()
+
+        orders = Order.objects.all()
+        self.assertEqual(orders.count(), 1)
+        self.assertEqual(orders[0].name, 'Apple Airpods 2')
+
