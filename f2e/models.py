17ef2b08b23133eaf6cfa0040aa0ff90d39733e4
xero7689 <volleyp7689@gmail.com>
2020-02-04 18:16:09 +0800

    Use PositiveSmallIntegerField implement choice of status code

--- f2e/models.py
+++ f2e/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from .static import ORDER_STATUS
 
 class Order(models.Model):
     name = models.TextField()
     logo = models.URLField()
-    status = models.IntegerField()
+    status = models.PositiveSmallIntegerField(choices=ORDER_STATUS)
     date = models.DateField()
---
+++ f2e/static.py
@@ -0,0 +1,8 @@
+ORDER_STATUS = (
+    (1, '處理中'), 
+    (2, '已成立'),
+    (3, '已取消'),
+    (4, '已送達'), 
+)
+
+ORDER_STATUS_DICT = dict(ORDER_STATUS)
