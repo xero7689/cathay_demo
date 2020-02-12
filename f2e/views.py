0c7ec6700288bb0daac3f36b3263054b2e473872
xero7689 <volleyp7689@gmail.com>
2020-02-12 15:54:34 +0800

    Move business logic of deal_type from views to models by using custom serializer

--- f2e/views.py
+++ f2e/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from django.core import serializers
+#from django.core import serializers
 from django.template.loader import render_to_string
 
 from .models import Order
-from .static import ORDER_STATUS_DICT
+from .serializers import OrdersPythonSerializer
 
 def f2e(request):
     return render(request, 'f2e.html')
@@ -15,11 +15,8 @@ def orders(request):
     }
 
     if request.method == 'GET':
-        orders = Order.objects.all()
-        orders = serializers.serialize('python', orders)
-        for order in orders:
-            order['fields']['type'] = ORDER_STATUS_DICT[order['fields']['status']]
-            print(order)
-            response['orders'].append(order['fields'])
+        serializer = OrdersPythonSerializer()
+        orders = serializer.serialize(Order.objects.all())
+        response['orders'] = orders
 
     return JsonResponse(response)
